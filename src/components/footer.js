8c51cfbf3e03852c05b6ebd3fcbb8bbcc25a3e03
John Durso <johnxdurso@gmail.com>
2019-02-01 10:14:34 -0500

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => (
   <div className="footer">
         <p>
-            © {new Date().getFullYear()} developed by <a href="https://twitter.com/johndursodev">@johndursodev</a>
+            © {new Date().getFullYear()} developed by <a href="https://twitter.com/johndursodev" target="_blank">@johndursodev</a>
         </p>
   </div>
 )
